18a85978dfe911478323657cf5144aab98fbfbf4
Aaron Butler <butlera10@gmail.com>
2016-06-30 20:21:20 -0700

    Added guessing-game questions.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var player = prompt("What's your favorite NBA basketball player?:", "");
+/*var player = prompt("What's your favorite NBA basketball player?:", "");
 alert("Wow, "  + player + " " + "is a great player. He made a incredible mark in history!");
 
 var teamName = prompt("Do you have a favorite NBA team?:", "");
-alert("It's cool that you like" + " " + teamName+ " " + "but I would have to disagree lol!");
+alert("It's cool that you like the" + " " + teamName+ ", but I would have to disagree lol!");
 
 var moment = prompt("Lastly, what's your best moment in your players career?:", "");
 alert("That was a great moment in his career!");
@@ -21,8 +21,8 @@ if (hourNow > 18) {
     greeting = 'Welcome!';
 }
 
-var message = "Welcome to my web page";
-message = message+ "It's a work in progress.";
+var message = "Welcome to my web page;";
+message = message+ " It's a work in progress.";
 var today = new Date();
 today = "Today is " + "" + today;
 alert(message);
@@ -31,4 +31,32 @@ console.log(today);
 
 
 
-document.write('<h3>' + greeting + '</h3>');
+document.write('<h3>' + greeting + '</h3>');*/
+
+
+var brand = "nike";
+var favoriteBrand = prompt("What's my favorite brand?");
+if(favoriteBrand === "nike"){
+  alert("That's correct!");
+}else{
+  alert("That's not correct!");
+}
+  console.log(brand);
+
+  var car = "lexus";
+  var favoriteCar = prompt("What's my favorite car make: BMW, Chevorlet, or Lexus?");
+  if(favoriteCar === car){
+    alert("That's correct!");
+  }else{
+    alert("That's not correct!");
+  }
+    console.log(car);
+
+    var pet = "dog";
+    var favoritePet = prompt("What's my favorite pet: cat, dog or horse?");
+    if(favoritePet === pet){
+      alert("That's correct!");
+    }else{
+      alert("That's not correct!");
+    }
+      console.log(pet);
