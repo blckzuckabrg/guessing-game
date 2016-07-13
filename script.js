7b2e224a720d03bc5f193bfa9ca9db50a5a11dff
Aaron Butler <butlera10@gmail.com>
2016-07-13 12:30:14 -0700

    Added css styling and images

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-
 var message = "Welcome soldier"+"";
 message = message+ " to The LBJ Guessing Game!";
+message += "Click the" + " "+"[Play Guessing Game]" +" "+ "button to continue!";
 var today = new Date();
 today = "Today is " + "" + today;
 alert(message);
@@ -9,7 +8,7 @@ document.write(today);
 console.log(today);
 
 
-var welcome = alert("Click the" + " "+"[Play Guessing Game]" +" "+ "button to continue!");
+
 function yourName() {
     var person = prompt("Please enter your name", "Billy Jean");
 
@@ -20,13 +19,15 @@ function yourName() {
 
     var myName = "Aaron";
     var player = prompt("What's the first name of this guessing game developer?");
+    var Construction = '<img src="like.png">';
+    var Construction1 = '<img src="dislike.png">';
     if((player.toLowerCase() == "Aaron") || (player.toLowerCase() == "aaron")) {
       alert("That's correct");
-      document.getElementById("1").textContent = "Correct: +1" ;
+      document.getElementById("1").innerHTML = "Correct: +1" + Construction ;
       document.getElementById("1").className = "correct";
     }else {
       alert("That's incorrect, my name is Aaron!");
-      document.getElementById("1").textContent = "Incorrect: -1" ;
+      document.getElementById("1").innerHTML = "Incorrect: -1" + Construction1 ;
       document.getElementById("1").className = "incorrect";
     }
 
@@ -35,11 +36,11 @@ function yourName() {
     var player = prompt("What's my favorite NBA basketball player's fullname?");
     if((player.toLowerCase() == "lebron james") || (player.toLowerCase() == "Lebron James")) {
       alert("That's correct");
-      document.getElementById("2").textContent = "Correct: +1" ;
+      document.getElementById("2").innerHTML = "Correct: +1" + Construction ;
       document.getElementById("2").className = "correct";
     }else {
       alert("Sorry, your guess was incorrect!");
-      document.getElementById("2").textContent = "Incorrect: -1";
+      document.getElementById("2").innerHTML = "Incorrect: -1" + Construction1;
       document.getElementById("2").className = "incorrect";
     }
 
@@ -47,84 +48,85 @@ function yourName() {
     var player = prompt("What's the fullname of the team that Lebron James play for?");
     if((player.toLowerCase() == "Cleveland Cavaliers") || (player.toLowerCase() == "cleveland cavaliers")) {
       alert("That's correct");
-      document.getElementById("3").textContent = "Correct: +1" ;
+      document.getElementById("3").innerHTML = "Correct: +1" + Construction ;
       document.getElementById("3").className = "correct";
     }else {
       alert("Sorry, your guess was incorrect!!");
-      document.getElementById("3").textContent = "Incorrect: -1";
+      document.getElementById("3").innerHTML = "Incorrect: -1" + Construction1;
       document.getElementById("3").className = "incorrect";
     }
 
     var dynasty = false;
     var championship = 3;
     var attempts = 0;
+    attempts++;
     while (dynasty == false) {
-      var player = prompt("How many championships does Lebron James have so far in 2016? ");
+      var player = prompt("How many championships does Lebron James have: 1, 2, 3 or 4?");
       if(player == 3 ){
         alert("That's correct!");
-        document.getElementById("4").textContent ="Correct: +1";
+        document.getElementById("4").innerHTML ="Correct: +1" + Construction;
         document.getElementById("4").className = "correct";
         dynasty = true;
 
-    }else{
-      alert("Guess was Incorrect!");
-      document.getElementById("4").textContent = "Attempts:", attempts++;
-      var player =prompt("How many championships does Lebron James have so far? ");
-    if(player < 3){
+    }else if(player < 3){
       alert("Ouch, close but guess is too low!");
-      document.getElementById("4").textContent = "Attempts:", attempts++;
+      document.getElementById("4").textContent = "Attempts:"+ attempts++;
       document.getElementById("4").className = "incorrect";
-    }else {
+    }else if(player > 3){
       alert("Sorry, guess is too high, try again!");
-      document.getElementById("4").textContent = "Attempts:", attempts++ ;
+      document.getElementById("4").textContent = "Attempts:"+ attempts++ ;
       document.getElementById("4").className = "incorrect";
     }
-  }
+}
+    var Construction = '<img src="like.png">';
+    var Construction1 = '<img src="dislike.png">';
+    var shoeDeal = prompt("What company did Lebron James sign a billion dollar deal with:  Adidas, Nike, or Puma ?");
+      if ((shoeDeal == "nike") || (shoeDeal == "Nike")) {
+        alert("That's correct");
+        document.getElementById("shoes").innerHTML = "Correct: +1" + Construction;
+        document.getElementById("shoes").className = "correct";
+
+      }else  {
+        alert("Sorry, your guess was incorrect!");
+        document.getElementById("shoes").innerHTML = "Incorrect: -1" + Construction1;
+        document.getElementById("shoes").className = "incorrect";
+      }
+
+      var finals = ["2007 Loss", "2011 Loss", "2012 Won", "2013 Won", "2014 Loss", "2015 Loss", "2016 Won"];
+      var finalsNum = finals.length;
+      var finalsYear = 0;
+      var msg = '';
+      var i;
+      //var Construction = '<img src="like.png">';
+      //var Construction1 = '<img src="dislike.png">';
+      var question = prompt("How many finals appearances does Lebron James have:\
+      choose a number (5 or 7)?");
+      if(question == 7){
+        alert("That's correct");
+        document.getElementById("correct").innerHTML =  "Correct: +1" + Construction;
+        document.getElementById("correct").className = "correct";
+
+
+      }else {
+        alert("Sorry, your guess was incorrect!");
+        document.getElementById("correct").innerHTML = "Incorrect: -1" + Construction1;
+        document.getElementById("correct").className = "incorrect";
+      }
+
 }
 
-var shoeDeal = prompt("What company did Lebron James sign a billion dollar deal with:  Adidas, Nike, or Puma ?");
-  if ((shoeDeal == "nike") || (shoeDeal == "Nike")) {
-    alert("That's correct");
-    document.getElementById("shoes").textContent = "Correct: +1";
-    document.getElementById("shoes").className = "correct";
-  }else  {
-    alert("Sorry, your guess was incorrect!");
-    document.getElementById("shoes").textContent = "Sorry, you guessed the wrong company!";
-    document.getElementById("shoes").className = "incorrect";
-  }
-
-  var finals = ["2007 Loss", "2011 Loss", "2012 Won", "2013 Won", "2014 Loss", "2015 Loss", "2016 Won"];
-  var finalsNum = finals.length;
-  var finalsYear = 0;
+  function appearances(){
   var msg = '';
   var i;
-  var question = prompt("How many finals appearances does Lebron James have:\
-  choose a number between 1-10?");
-  if(question == 7){
-    alert("That's correct");
-    document.getElementById("correct").textContent = "Correct: +1",   appearances();
-    document.getElementById("correct").className = "correct";
-
-
-  }else if (question > 7){
-    alert("That's incorrect");
-    document.getElementById("correct").textContent = "Sorry, your guess was incorrect!";
-    document.getElementById("correct").className = "incorrect";
-  }else {
-    alert("Sorry, your guess was incorrect!");
-    document.getElementById("correct").textContent = "Sorry, your guess was incorrect!!";
-    document.getElementById("correct").className = "incorrect";
-  }
-  function appearances(){
   for (var i = 0; i < finalsNum; i++) {
     finalsYear = ( i + 1);
     msg += 'NBA Finals Appearance' + finalsYear + ':' + " ";
     msg += finals[i] + '<br />';
-    document.getElementById("answer").innerHTML = msg;
-    document.getElementById("answer").className = "correct";
+    document.getElementById("correct").innerHTML = msg;
+
     }
   }
-}
+
 
 
 
